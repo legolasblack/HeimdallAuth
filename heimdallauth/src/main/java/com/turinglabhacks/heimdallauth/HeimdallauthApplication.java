c8efd7781eec21468ef3954e322e7ad098f3254a
legolasblack <legolas.black.ds@gmail.com>
2025-05-01 15:53:11 -0600

    primera version de carpetas, antes de integrar docker

---
+++ heimdallauth/src/main/java/com/turinglabhacks/heimdallauth/HeimdallauthApplication.java
@@ -0,0 +1,13 @@
+package com.turinglabhacks.heimdallauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeimdallauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeimdallauthApplication.class, args);
+	}
+
+}
---
+++ heimdallauth/src/test/java/com/turinglabhacks/heimdallauth/HeimdallauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turinglabhacks.heimdallauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeimdallauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
