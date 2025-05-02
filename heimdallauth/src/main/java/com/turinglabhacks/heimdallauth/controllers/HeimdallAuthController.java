1cbd709716a103c55fef3ae953b5d922bb7e2c2b
legolasblack <legolas.black.ds@gmail.com>
2025-05-01 21:13:35 -0600

    Agregamos tres endpoints de prueba para hacer las integraciones de swagger y demas

--- heimdallauth/src/main/java/com/turinglabhacks/heimdallauth/controllers/HeimdallAuthController.java
+++ heimdallauth/src/main/java/com/turinglabhacks/heimdallauth/controllers/HeimdallAuthController.java
@@ -2,11 +2,31 @@ package com.turinglabhacks.heimdallauth.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/v1")
 public class HeimdallAuthController {
 
 
+    @GetMapping("/helloword/{name}")
+    public String getMethodName(@PathVariable String name) {
+        return "hello word " + name;
+    }
+    
+    
+    @GetMapping("/helloword")
+    public String gethelloworde() {
+        return "hello word ";
+    }
+
+    @GetMapping("/perfil/{idUser}")
+    public ResponseEntity<String> obtenerPerfil(@PathVariable String idUser) {
+        return ResponseEntity.ok().body("hello " + idUser);
+    }
     
 }
